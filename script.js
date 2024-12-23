ada49cdf48e3f7281e022b62d5d95ef50bbd950a
Ihor Bezprozvannyi <bi092006@gmail.com>
2024-12-23 13:43:39 +0100

    gallery update

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 function galleryOn(){
-    $('.slide2').fadeIn(1000, changeImage())
+    console.log('zmiana');
+    
+    $('.slide2').animate({
+        opacity: 0,
+      }, 2000, changeImage);
     console.log('fadeout')
 }
 
 var images = ['sample1.jpg', 'sample2.jpg', 'sample3.jpg', 'sample4.jpg']
-var current = 0 
-var next = 1
+var current = -1 
+var next = 0
 
 function changeImage(){
     current = (current + 1) % images.length;
+    console.log(current)
     $('.slide2').attr('src', "img/" + images[current])
     console.log('change1')
-
+    $('.slide2').css('opacity', 1);
     next = (current + 1) % images.length
+    console.log(next)
     $('.slide1').attr('src', "img/" + images[next])
     console.log('change2')
-
-    $('.slide2').fadeOut()
-    console.log('fadein')
     
     setTimeout('galleryOn()', 3000)
 }
